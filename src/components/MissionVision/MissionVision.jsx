87c27280e21289eedc028f49791bb1c5d3fda088
Aditya200410 <adityakumar098k@gmail.com>
2025-07-01 01:06:32 +0530

    fix

--- src/components/MissionVision/MissionVision.jsx
+++ src/components/MissionVision/MissionVision.jsx
@@ -29,7 +29,7 @@ const cardVariants = {
 
 export default function MissionVision() {
   return (
-    <section className="py-6 md:py-10 lg:py-12 bg-pink-600">
+    <section className="py-6 md:py-10 lg:py-12">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
